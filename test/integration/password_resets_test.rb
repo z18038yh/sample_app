5e21ad46a14d6af0a95edd7897021d9bace3142f
z18038yh <51823732+z18038yh@users.noreply.github.com>
2020-06-14 11:19:57 +0000

    Delete unnecessary comments

--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -1,9 +1,6 @@
 require 'test_helper'
 
 class PasswordResetsTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
   def setup
     ActionMailer::Base.deliveries.clear
     @user = users(:michael)
